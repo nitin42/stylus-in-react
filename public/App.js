fc2a1155974da6924ff7526ac638a2cbebdbeb94
Nitin <tulswani19@gmail.com>
2017-09-12 02:02:27 +0530

    sample example

---
+++ public/App.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import createStylusComponent from '../build/main';
+
+const Container = createStylusComponent`
+para-color = mistyrose
+
+div
+  display flex
+  flex-direction column
+  justify-content center
+  align-items center
+  padding 20px
+  margin 10px
+  >p
+    color darken(para-color, 10)
+`
+
+const Input = createStylusComponent`
+input
+  padding 4px
+  width 80px
+  border 1px solid pink
+  border-radius 3px
+  >p
+    color mistyrose
+  &:focus
+    outline none
+  &:hover
+    width percentage(.15)
+`
+
+const Button = createStylusComponent`
+choices = 3px 4px 5px 7px
+
+border-radius(n)
+  border-radius n
+
+button
+  margin-top 20px
+  border-radius(8px)
+  border 1px solid mistyrose
+  background white
+  color black
+  padding choices[3]
+  &:hover
+    background darken(mistyrose, 10)
+    color white
+  &:focus
+    outline none
+  cursor pointer
+`
+
+const Image = createStylusComponent`
+image-styles(width, height, border, radius)
+  margin-top 30px
+  width width
+  height height
+  border border solid mistyrose
+  border-radius radius
+
+img
+  image-styles(200, 200, 3px, 4px)
+`
+
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Input type="text" name="sample" />
+        <Image src='http://365.unsplash.com/assets/paul-jarvis-9530891001e7f4ccfcef9f3d7a2afecd.jpg' />
+        <Button>Submit</Button>
+      </Container>
+    );
+  }
+}
+
+render(<App />, document.getElementById('app'));
