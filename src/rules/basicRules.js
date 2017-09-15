906835080049f4656cf467fe8887be0c4e421e84
Nitin <tulswani19@gmail.com>
2017-09-15 22:22:57 +0530

    fixed lint issues

---
+++ src/rules/basicRules.js
@@ -0,0 +1,37 @@
+const prefixAll = require("inline-style-prefixer/static");
+const camelCase = require("camel-case");
+
+const types = require("../utils/ruleTypes");
+
+/**
+ * This function returns an array of all the style rules
+ * @param { array } rules
+ * @param { string } root
+ */
+function getRules(rules, root) {
+  const arr = [];
+
+  rules.forEach(rule => {
+    if (!types.includes(rule.type)) {
+      if (rule.selectors[0] === root) {
+        rule.declarations.forEach(decl => {
+          arr.push(prefixAll({ [camelCase(decl.property)]: decl.value }));
+        });
+      } else {
+        rule.declarations.forEach(decl => {
+          arr.push(
+            prefixAll({
+              [rule.selectors[0].replace(root, "")]: {
+                [camelCase(decl.property)]: decl.value
+              }
+            })
+          );
+        });
+      }
+    }
+  });
+
+  return arr;
+}
+
+module.exports = getRules;
---
+++ src/rules/supportsRules.js
@@ -0,0 +1,19 @@
+const getRules = require("./basicRules");
+
+/**
+ * This function returns an array of objects of @supports rules
+ * @param { array } rules @supports rules
+ * @param { string } root root selector
+ */
+function supportRules(rules, root) {
+  const arr = [];
+
+  rules.forEach(rule => {
+    if (rule.type !== "supports") return;
+    arr.push(getRules(rule.rules, root));
+  });
+
+  return arr;
+}
+
+module.exports = supportRules;
