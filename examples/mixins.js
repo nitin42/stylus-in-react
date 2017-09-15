defa9b0d644a5ecf93ee6f71fd6ab56ebb30e445
Nitin <tulswani19@gmail.com>
2017-09-15 22:17:43 +0530

    init

---
+++ examples/mixins.js
@@ -0,0 +1,17 @@
+const { stylus } = require("../src/");
+
+const ButtonMixin = stylus(
+  `
+button-styles(radius, size, color, type)
+  border-radius radius
+  border size type color
+  color mistyrose
+  background-color white
+
+button
+  button-styles(3px, 2px, mistyrose, solid)
+`,
+  { displayName: "MixinButton" }
+);
+
+module.exports = ButtonMixin;
