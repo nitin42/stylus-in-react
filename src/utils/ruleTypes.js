5c5a616e17a055b498adfeb32b272c9a53ea0f9e
Nitin <tulswani19@gmail.com>
2017-09-15 22:23:08 +0530

    fixed lint issueS

---
+++ src/utils/ruleTypes.js
@@ -0,0 +1,16 @@
+/**
+ * Keep a track of type of style rule (currently, only some of them are implemented)
+ */
+module.exports = [
+  "media",
+  "keyframes",
+  "font-face",
+  "supports",
+  "page",
+  "namespace",
+  "import",
+  "host",
+  "document",
+  "custom-media",
+  "charset"
+];
