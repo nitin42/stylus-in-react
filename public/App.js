06a4baffd58fb0c5e2b27e6cb32fa25a6273d138
Nitin <tulswani19@gmail.com>
2017-09-15 23:20:21 +0530

    added font-face example

--- public/App.js
+++ public/App.js
@@ -9,10 +9,11 @@ const ButtonVar = require("../examples/variables");
 const ButtonMedia = require("../examples/media");
 const FadeInButton = require("../examples/animation");
 const ButtonFloat = require("../examples/supports");
+const Name = require("../examples/fontFace");
 
 class App extends React.Component {
   render() {
-    return <Button>Submit</Button>;
+    return <FadeInButton>Submit</FadeInButton>;
   }
 }
 
