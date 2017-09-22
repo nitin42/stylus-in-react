f64cf08530fb3909cf983ed8b7b0dc5ae65ceaae
Nitin <tulswani19@gmail.com>
2017-09-22 13:28:17 +0530

    Init commit

---
+++ webpack/webpack.config.dev.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: path.resolve(__dirname, '../public/App.js'),
+  output: {
+    filename: '../bundle.js',
+    path: path.resolve(__dirname),
+    publicPath: '/'
+  },
+  node: {
+    fs: 'empty'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: ['node_modules'],
+        use: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        comparisons: false
+      },
+      output: {
+        comments: false,
+        ascii_only: true
+      }
+    })
+  ],
+  devtool: 'cheap-eval-source-maps'
+}
